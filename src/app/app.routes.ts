0b03f758fc6eb6e0a69986890ca41eb68f742193
NAME <EMAIL ADDRESS>
2024-05-06 16:57:56 -0400

    Lazy load standalone components; split into two distinct paths; have fallback if user goes outside legit paths

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,21 @@
 import { Routes } from '@angular/router';
 import { JobComponent } from './components/job/job.component';
+import { DetailedJobComponent } from './components/detailed-job/detailed-job.component';
 
-export const routes: Routes = [{ path: 'jobs', component: JobComponent }];
+export const routes: Routes = [
+  {
+    path: 'jobs',
+    loadComponent: () =>
+      import('./components/job/job.component').then(
+        (module) => module.JobComponent
+      ),
+  },
+  {
+    path: 'jobs/:id',
+    loadComponent: () =>
+      import('./components/detailed-job/detailed-job.component').then(
+        (module) => module.DetailedJobComponent
+      ),
+  },
+  { path: '**', redirectTo: '/jobs' },
+];
